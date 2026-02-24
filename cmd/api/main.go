20c6623a069f4f9ceae3d254fcc29534e3d02bea
Pegasis1002 <thewebserfer1234@gmail.com>
2026-02-24 23:10:39 +0530

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []user{
+	{ID: "1", Name: "11"},
+	{ID: "2", Name: "22"},
+	{ID: "3", Name: "33"},
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/users", get_users)
+
+	r.Run("localhost:8080")
+}
+
+func get_users(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
